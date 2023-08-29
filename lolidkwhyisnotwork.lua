908d1dbb06a406ce8ee52a2859ffd5a8424a0001
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 20:23:32 +0300

    Update lolidkwhyisnotwork.lua

--- lolidkwhyisnotwork.lua
+++ lolidkwhyisnotwork.lua
@@ -3,8 +3,8 @@ local Achievements = {}
 Achievements.Get = function(data)
 	task.spawn(function()
 
-
 		local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
+	        v1.Name = "SHELLHELL"
 		v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
 		v1.Info.KnobBonus.Text = data.KnobBonus
 		game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers.SHELLHELL.Text = data.Modifiers
