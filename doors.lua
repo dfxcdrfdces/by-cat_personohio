ee5ebd6c84ab438e76159d47bed4519a2e9d4e1f
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 16:25:19 +0300

    Create doors.lua

---
+++ doors.lua
@@ -0,0 +1,8 @@
+Creator.CustomDialogue = function(Config)
+local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
+v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
+v1.Info.KnobBonus.Text = data.KnobBonus
+v1.Locked.Background.Image = LoadCustomAsset(Config.Image)
+v1.Text = data.v1
+end
+return Creator
