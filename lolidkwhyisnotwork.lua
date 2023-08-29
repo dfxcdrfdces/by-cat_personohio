4afb6644e4ff349e7b7d6fad14b39ec240012007
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 17:04:17 +0300

    Update lolidkwhyisnotwork.lua

--- lolidkwhyisnotwork.lua
+++ lolidkwhyisnotwork.lua
@@ -1,13 +1,17 @@
 local Achievements = {}
 
 Achievements.Get = function(data)
-		    task.spawn(function()
+	task.spawn(function()
 
-local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
-v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
-v1.Info.KnobBonus.Text = data.KnobBonus
-v1.Locked.Background.Image = LoadCustomAsset(data.Image)
-v1.Text =  data.v1
+
+	local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
+	v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
+	v1.Info.KnobBonus.Text = data.KnobBonus
+	v1.Locked.Background.Image = LoadCustomAsset(data.Image)
+	v1.Text =  data.v1
+
+	end)
+
 end
-	end
-return Creator
+
+	return Achievements
