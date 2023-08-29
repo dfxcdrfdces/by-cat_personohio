e58bd12488dd7af55d1d27c88c73b92e662d6b6b
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 20:17:41 +0300

    Update lolidkwhyisnotwork.lua

--- lolidkwhyisnotwork.lua
+++ lolidkwhyisnotwork.lua
@@ -7,6 +7,7 @@ Achievements.Get = function(data)
 		local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
 		v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
 		v1.Info.KnobBonus.Text = data.KnobBonus
+		game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers.SHELLHELL.Text = data.Modifiers
 		v1.Locked.Background.Image = data.Image
 		local v2 = v1.Text == data.v2
 
