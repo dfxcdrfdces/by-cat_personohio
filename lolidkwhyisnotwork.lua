bd13ad7bbd30d7e95f15bb43385d66e850a2c69e
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 16:45:48 +0300

    Update lolidkwhyisnotwork.lua

--- lolidkwhyisnotwork.lua
+++ lolidkwhyisnotwork.lua
@@ -1,10 +1,13 @@
 local Creator = {}
 
 Creator.CustomDialogue = function(Config)
+	    task.spawn(function()
+
 	local v1 = game:GetObjects("rbxassetid://14611931575")[1] 
 	v1.Parent = game.Players.LocalPlayer.PlayerGui.MainUI.LobbyFrame.CreateElevator.Modifiers
 	v1.Info.KnobBonus.Text = Config.KnobBonus
 	v1.Text = Config.v1
+		end
 end
 return Creator
 
