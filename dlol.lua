2f7247a532fd73a35228e0235f5087d322aab4d1
dfxcdrfdces <127351171+dfxcdrfdces@users.noreply.github.com>
2023-08-29 16:31:57 +0300

    Create dlol.lua

---
+++ dlol.lua
@@ -0,0 +1,7 @@
+local here = loadstring(game:HttpGet("https://raw.githubusercontent.com/dfxcdrfdces/by-cat_personohio/main/doors.lua"))()
+
+here.CustomDialogue({
+ Image = "rbxassetid://14567452788",
+ v1.Text = "brooo",
+ KnobBonus.Text = "yooo",
+    })
